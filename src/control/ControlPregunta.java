51a6184bd939360dbbe30514952e69d22298cb10
chrissocoro <48024564+chrissocoro@users.noreply.github.com>
2020-05-20 21:52:37 -0500

    dded

--- src/control/ControlPregunta.java
+++ src/control/ControlPregunta.java
@@ -5,8 +5,12 @@
  */
 package control;
 
+import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.Pregunta;
+import modelo.Profesor;
 
 /**
  *
@@ -41,10 +45,26 @@ public class ControlPregunta {
         
     }
 
-  
-
-   
-    
-    
-    
+//  public LinkedList<Pregunta> consultarPregunta() {
+//
+//        LinkedList<Profesor> ma = new LinkedList<>();
+//        //String sql = "select codigoP,nombreP1,nombreP2,apellidoP1,apellidoP2,telefonoP1,telefonoP2,correoP,contraseñaP,dirrecionP";
+//        String sql = "select * from pregunta";
+//        Pregunta obja = new Pregunta();
+//        ma = obja.ListraProfesores(sql);
+////        try {
+////            
+////            ma = obja.ListraProfesores(sql);
+////            
+////        } catch (SQLException ex) {
+////            Logger.getLogger(ControlProfesor.class.getName()).log(Level.SEVERE, null, ex);
+////        }
+////
+//        return ma;
+//
+//   
+//    
+//    
+//    
+//}
 }
